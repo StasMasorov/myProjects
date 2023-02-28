6e34fb28449fb527cf01886aa078d286bdb5ad03
Stas Masorov <stas.masorov@mail.ru>
2023-02-28 16:57:32 +0500

    try/catch

---
+++ exercises/JSOOP/tryCatch.js
@@ -0,0 +1,18 @@
+export default class ParseError extends Error {
+    constructor(message) {
+      super(message);
+      this.name = 'ParseError';
+    }
+  }
+  
+
+import ParseError from './ParseError.js';
+
+export function parseJson(json) {
+    try {
+      return JSON.parse(json);
+    }
+    catch (e) {
+      throw new ParseError('Invalid JSON string');
+    }
+  };
